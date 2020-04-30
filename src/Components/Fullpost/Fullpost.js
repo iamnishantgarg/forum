f1a0b0a904978d11a83a535174a2e9109c74939e
Nishant garg <iamnishantgarg@gmail.com>
2020-04-30 05:31:01 +0530

    basic setup

--- src/Components/Fullpost/Fullpost.js
+++ src/Components/Fullpost/Fullpost.js
@@ -1,7 +1,17 @@
-import React, { Component } from "react";
-import classes from "./Fullpost.module.css";
-export default class Fullpost extends Component {
+import React, { Component } from "./node_modules/react";
+import "./FullPost.css";
+export default class FullPost extends Component {
   render() {
-    return <div></div>;
+    let post = <p>Please select a Post!</p>;
+    post = (
+      <div className="FullPost">
+        <h1>Title</h1>
+        <p>Content</p>
+        <div className="Edit">
+          <button className="Delete">Delete</button>
+        </div>
+      </div>
+    );
+    return post;
   }
 }
