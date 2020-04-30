57b15ab8570c66c7e69c4ca975f425121bcfc549
Nishant garg <iamnishantgarg@gmail.com>
2020-04-30 05:39:12 +0530

    basic setup

--- src/Components/Fullpost/Fullpost.js
+++ src/Components/Fullpost/Fullpost.js
@@ -1,4 +1,4 @@
-import React, { Component } from "./node_modules/react";
+import React, { Component } from "react";
 import "./FullPost.css";
 export default class FullPost extends Component {
   render() {
